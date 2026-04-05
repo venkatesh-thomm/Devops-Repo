f39262322c898fb0c0a1a2c660979cc17d38a609
Venkatesh Thommandru <venkatesh.thomm@gmail.com>
2026-04-05 17:52:58 +0530

    Updated

---
+++ Terraform/data/data.tf
@@ -0,0 +1,24 @@
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+  owners = ["099720109477"]
+}
+
+output "ami_id" {
+  value = data.aws_ami.ubuntu.id
+}
+
+
